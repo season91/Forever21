2e393f3eb9eae3c91fe16750aea084a04781552a
성민 유 <usm9401@gmail.com>
2025-05-08 20:53:01 +0900

    유성민 : 아이템 생성

---
+++ Assets/01.Scripts/InteractiveItem/InteractiveItem.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveItem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    BoxCollider2D boxColider2D;
+    SpriteRenderer spriteRenderer;
+
+    delegate void OnCollisionEnter(Collider2D other);
+
+    OnCollisionEnter onCollisionEnter;
+
+    private void Reset()
+    {
+        boxColider2D = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            onCollisionEnter(collision);
+        }   
+    }
+
+
+
+}
